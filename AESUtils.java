feefeda204b71be53bf4f3e530eabca3c0c23410
戴超 <424360793@qq.com>
2017-11-25 23:20:23 +0800

    [*]加密解密算法 并且防止linux和windows生成key不一致

--- AESUtils.java
+++ AESUtils.java
@@ -4,26 +4,23 @@
  * AES加密算法
  */
 public class AESUtils {
-    private final static Logger logger = LoggerFactory.getLogger(AESUtils.class);
 
     /**
      * AES加密字符串
      *
      * @param content  需要被加密的字符串
+     * @param password 加密需要的密码
      * @return 密文
      */
-    public static String encryptAES(String content,String rawPassword) {
-        String password;
+    public static String encryptAES(String content, String password) {
         try {
-            //生成password
-            MessageDigest md5 = MessageDigest.getInstance("MD5");
-            BASE64Encoder base64en = new BASE64Encoder();
-            //加密后的字符串
-            password  = base64en.encode(md5.digest(rawPassword.getBytes("utf-8")));
-
             KeyGenerator kgen = KeyGenerator.getInstance("AES");// 创建AES的Key生产者
 
-            kgen.init(128, new SecureRandom(password.getBytes()));// 利用用户密码作为随机数初始化出
+            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG" );
+            secureRandom.setSeed(password.getBytes());
+            kgen.init(128,secureRandom);
+            // 128位的key生产者
+            //加密没关系，SecureRandom是生成安全随机数序列，password.getBytes()是种子，只要种子相同，序列就一样，所以解密只要有password就行
 
             SecretKey secretKey = kgen.generateKey();// 根据用户密码，生成一个密钥
 
@@ -40,9 +37,44 @@ public class AESUtils {
             return parseByte2HexStr(cipher.doFinal(byteContent));
 
         } catch (Exception e) {
-            logger.error("加密出错,内容={}，密码={},异常={}", content, LogExceptionStackTrace.erroStackTrace(e));
+            logger.error("加密出错,内容={}，密码={},异常={}", content, password, LogExceptionStackTrace.erroStackTrace(e));
         }
         return "-1";
+    }
+
+    /**
+     * 解密AES加密过的字符串
+     *
+     * @param content  AES加密过过的内容
+     * @param password 加密时的密码
+     * @return 明文
+     */
+    public static byte[] decrypt(byte[] content, String password) {
+        try {
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");// 创建AES的Key生产者
+            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG" );
+            secureRandom.setSeed(password.getBytes());
+            kgen.init(128,secureRandom);
+            SecretKey secretKey = kgen.generateKey();// 根据用户密码，生成一个密钥
+            byte[] enCodeFormat = secretKey.getEncoded();// 返回基本编码格式的密钥
+            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");// 转换为AES专用密钥
+            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
+            cipher.init(Cipher.DECRYPT_MODE, key);// 初始化为解密模式的密码器
+            return cipher.doFinal(content); // 明文
+
+        } catch (Exception e) {
+            logger.error("解密出错,内容={}，密码={},异常={}", content, password, LogExceptionStackTrace.erroStackTrace(e));
+        }
+        return "-1".getBytes();
+    }
+
+    //解密算法
+    public static String decryptAES(String content, String password) {
+        byte[] decrypt = decrypt(parseHexStr2Byte(content), password);
+        if (decrypt == null) {
+            return "-1";
+        }
+        return new String(decrypt);
     }
 
 
@@ -62,6 +94,24 @@ public class AESUtils {
             sb.append(hex.toUpperCase());
         }
         return sb.toString();
+    }
+
+    /**
+     * 将16进制转换为二进制
+     *
+     * @param hexStr
+     * @return
+     */
+    public static byte[] parseHexStr2Byte(String hexStr) {
+        if (hexStr.length() < 1)
+            return null;
+        byte[] result = new byte[hexStr.length() / 2];
+        for (int i = 0; i < hexStr.length() / 2; i++) {
+            int high = Integer.parseInt(hexStr.substring(i * 2, i * 2 + 1), 16);
+            int low = Integer.parseInt(hexStr.substring(i * 2 + 1, i * 2 + 2), 16);
+            result[i] = (byte) (high * 16 + low);
+        }
+        return result;
     }
 
 }
